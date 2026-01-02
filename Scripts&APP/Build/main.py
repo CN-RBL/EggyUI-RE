69930277515c10d6a127dc8933f342cf124dfca7
CN-RBL <red.blue.light@qq.com>
2026-01-02 22:30:41 +0800

    @ALL 明天我要比赛了
    保AC

--- Scripts&APP/Build/main.py
+++ Scripts&APP/Build/main.py
@@ -5,9 +5,12 @@ import argparse
 import warnings
 import logging
 import subprocess as sp
+import json
+import os
 
 warnings.filterwarnings("ignore")
 
+# TODO: 信仰之蛋
 eu_logo = """[bold]
 [yellow]                                                     .+:        
                                                      +%#:       
@@ -75,24 +78,40 @@ eure_build = """
 """
 
 
-def main():
+def main() -> None:
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(message)s",
         datefmt="%Y/%m/%d %I:%M:%S",
         handlers=[RichHandler(locals_max_length=None, locals_max_string=None)]
     )
-    log = logging.getLogger("rich")
+    log: logging.Logger = logging.getLogger("rich")
 
-    console = rich.console.Console()
+    console: rich.console.Console = rich.console.Console()
     console.print(eu_logo)
     console.print(eure_build)
 
     log.debug("初始化console完成")
 
-    parser = argparse.ArgumentParser(description="Eggy UI RE Build Tool")
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Eggy UI RE Build Tool")
     parser.add_argument("build_config", help="编译配置")
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
     log.debug(f"获取参数：build_config，参数值为{args.build_config}")
     log.info("准备编译...")
+
+    config_path: str = f"{os.path.dirname(os.path.realpath(__file__))}/{args.build_config}.json"
+    with open(config_path, "rt", encoding="utf-8") as f:
+        log.debug(f"编译配置文件路径：{config_path}")
+        config: dict = json.load(f)
+        log.info(f"编译配置文件加载完成")
+        log.info(f"名称：{config["config_name"]}\n"
+                 f"描述：{config["config_description"]}\n"
+                 f"异步：{"是" if config["async"] else "否"}")
+        try:
+            _: int = 1
+            while True:
+                log.debug(f"第{_}个配置项：{config[f"M{_}"]}")
+                _ += 1
+        except KeyError as e:
+            pass
