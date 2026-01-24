6fa633b2889dc7e7223771cd2877bf7ff0bba878
CN-RBL <red.blue.light@qq.com>
2026-01-24 17:33:13 +0800

    @Sylphy-0xd3ac @SYSTEM-LIGHT
    来

--- Scripts&APP/DesktopPet/pet.py
+++ Scripts&APP/DesktopPet/pet.py
@@ -5,14 +5,33 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 from util import get_path
 
+class Emoticon_Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("")
+        self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
+        self.setFixedSize(100, 100)
+    
+    def set_emoticon(self, emoticon_path: str):
+        """
+        通过此方法设置表情气泡中的表情图片
+        :param emoticon_path: 表情图片路径
+        """
+        pass
+
 class Pet(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Your Eggy")
         self.setWindowIcon(QIcon(get_path(__file__, "icon.ico")))
-        # self.setWindowFlag(self.windowFlags() | Qt.WindowType.FramelessWindowHint)
+        self.setWindowFlag(self.windowFlags() | Qt.WindowType.FramelessWindowHint)
         self.setFixedSize(350, 350)
 
-    def talk(self):  pass
+    def talk(self):
+        """
+        通过此方法进入AI讨论页面
+        """
+        pass
     def settings(self): pass
-    def exit(self):  exit(0)
+    def exit(self):
+        exit()
---
+++ Scripts&APP/EggyAI/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# TODO: For Sylphy/SYSTEM-LIGHT:
+# Get a API KEY from api.chatanywhere.org, Let us develop the EggyAI together!
+# PS: Use FREE
+# PS: 想办法解决一下跨程序交流问题：
+"""
+如：
+Pet to AI
+Pet.setting to Center
+"""
