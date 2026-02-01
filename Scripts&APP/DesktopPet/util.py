cd4b51f917ffb24d3042c5d8db8011c16e03158e
CN-RBL <red.blue.light@qq.com>
2026-02-01 22:53:46 +0800

    Update util.py

--- Scripts&APP/DesktopPet/util.py
+++ Scripts&APP/DesktopPet/util.py
@@ -2,4 +2,10 @@ from os.path import abspath, dirname
 
 
 def get_path(relative_path: str, filename: str) -> str:
+    """
+    返回relative_path所在目录的filename地址
+    :param relative_path:
+    :param filename:
+    :return: str
+    """
     return f"{dirname(abspath(relative_path))}\\{filename}"
